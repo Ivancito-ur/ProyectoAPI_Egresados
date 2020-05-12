1d741e5d3224283da794c88fde9aceac84acee27
nicolaspa1 <38383906+nicolaspa1@users.noreply.github.com>
2020-05-11 19:11:41 -0500

    hacer cambios al nombre de la base de datos en esta clase

--- metodos/hoja_vida/Conexion.php
+++ metodos/hoja_vida/Conexion.php
@@ -13,6 +13,7 @@ class Conexion {
         try {
          //  $pdo = new PDO("{$this->driver}:host={$this->host};dbName={$this->dbName};charset={$this->charset}", $this->user, $this->pass);
            $pdo = new PDO('mysql:host=localhost;dbname=f;charset=utf8', 'root', '');
+            //En la variable pdo cambiar el nombre de la base de datos (Actualmente le puse un f para efectos practicos de una prueba)
            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          //  echo "{$this->driver}:host={$this->host};dbName={$this->dbName};charset={$this->charset}", "{$this->user}", $this->pass;
             return $pdo;
