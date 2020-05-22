5d0de2e55ab94248c2625023c52926a5ab525c7f
Ivancito-ur <61300455+Ivancito-ur@users.noreply.github.com>
2020-05-22 12:58:50 -0500

    Validacion formato excel

--- vista/director/cargar.php
+++ vista/director/cargar.php
@@ -6,18 +6,17 @@
                 <br>
                 <h4 style="padding-left: 10px;">Actualizar Estudiante</h4>
                 <div class="input-group mb-3" style="padding-left: 10px; padding-right: 10px;">
-                        <input type="number" id="busquedaCodigo" class="form-control" placeholder="Codigo Estudiante"
-                            aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <div class="input-group-append">
-                            <button onclick="cargaDatos(event)" class="btn btn-outline-secondary" type="button" id="button-addon2" style="background-color: #dd4b39; border-color: #dd4b39; color: white;">Buscar</button>
-                        </div>
+                    <input type="number" id="busquedaCodigo" class="form-control" placeholder="Codigo Estudiante" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button onclick="cargaDatos(event)" class="btn btn-outline-secondary" type="button" id="button-addon2" style="background-color: #dd4b39; border-color: #dd4b39; color: white;">Buscar</button>
                     </div>
-                    <div style="width:80%;margin:auto; display:none; text-align:center; padding:10px ; margin-top:15px" id="alertCodigo" class="alert alert-danger" role="alert">
-              <p class="respuesta" id="respuestaCarga"></p>
-            </div>
-            <div style="width:80%; margin:auto;display:none;  text-align:center; padding:10px ; margin-top:15px" id="alert2Codigo" class="alert alert-success" role="alert">
-              Cargado Correctamente
-            </div>
+                </div>
+                <div style="width:80%;margin:auto; display:none; text-align:center; padding:10px ; margin-top:15px" id="alertCodigo" class="alert alert-danger" role="alert">
+                    <p class="respuesta" id="respuestaCarga"></p>
+                </div>
+                <div style="width:80%; margin:auto;display:none;  text-align:center; padding:10px ; margin-top:15px" id="alert2Codigo" class="alert alert-success" role="alert">
+                    Cargado Correctamente
+                </div>
                 <form id="fcargaDatos">
                     <div class="form-group">
                         <label for="nombre">Nombre</label>
@@ -41,14 +40,13 @@
                     </div>
 
                     <div style="width:80%;margin:auto; display:none; text-align:center; padding:10px; margin-bottom:15px" id="actu1" class="alert alert-danger" role="alert">
-                    <p class="respuesta" id="respuestaActualizar"></p>
+                        <p class="respuesta" id="respuestaActualizar"></p>
                     </div>
                     <div style="width:80%; margin:auto;display:none;  text-align:center; padding:10px ; margin-top:15px; margin-bottom:15px" id="actu2" class="alert alert-success" role="alert">
-                    Actualizado Correctamente
+                        Actualizado Correctamente
                     </div>
-                    
-                    <button type="submit" class="btn btn-primary"
-                        id="botonActualizar" style="background-color: #dd4b39; border-color: #dd4b39;"  onclick="return actualizarFecha(event)">Actualizar </button>
+
+                    <button type="submit" class="btn btn-primary" id="botonActualizar" style="background-color: #dd4b39; border-color: #dd4b39;" onclick="return actualizarFecha(event)">Actualizar </button>
                 </form>
             </div>
         </div>
@@ -56,24 +54,28 @@
             <div style="border-top: 3px solid #3c8dbc; background-color: white; padding-bottom: 10px;">
                 <br>
                 <h4 style="padding-left: 10px;">Seleccionar Archivo</h4>
-                <form  action="" class="formularioCompleto" method="" enctype="multipart/form-data">
+                <form action="" class="formularioCompleto" method="" enctype="multipart/form-data">
                     <div class="input-group mb-3" style="padding-left: 10px; padding-right: 10px;">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"  id="inputGroupFileAddon01">Cargar</span>
+                            <span class="input-group-text" id="inputGroupFileAddon01">Cargar</span>
                         </div>
                         <div class="custom-file">
-                            <input onchange="cargaHojaVida()" type="file" style="display:none"class="custom-file-input" id="inputGroupFile01"
-                                aria-describedby="inputGroupFileAddon01"  name="archivo">
-                            <label  class="custom-file-label" for="inputGroupFile01"><p class="nameArchivo">...</p></label>
+                            <input onchange="cargaHojaVida()" type="file" style="display:none" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" name="archivo">
+                            <label class="custom-file-label" for="inputGroupFile01">
+                                <p class="nameArchivo">...</p>
+                            </label>
                         </div>
                     </div>
-                    <button type="button" id="guardaExcel" class=" btn btn-primary" onclick="cargarExcel(event, '#guardaExcel')"
-                        style="background-color: #dd4b39; border-color: #dd4b39;">Guardar</button>
-                        <div style="display:none; text-align:center; padding:10px ; margin-top:15px" id="alert"class="alert alert-danger" role="alert">
-                        <p class="respuesta" id="respuesta" ></p></div>
-                        <div style="display:none; text-align:center; padding:10px ; margin-top:15px" id="alert2" class="alert alert-success" role="alert">
+                    <button type="button" id="guardaExcel" class=" btn btn-primary" onclick="cargarExcel(event, '#guardaExcel')" style="background-color: #dd4b39; border-color: #dd4b39;">Guardar</button>
+                    <div style="display:none; text-align:center; padding:10px ; margin-top:15px" id="alert" class="alert alert-warning alert-dismissible fade show"
+                     role="alert">
+                        <p class="respuesta" id="respuesta"></p>
+                    </div>
+                    <div style="display:none; text-align:center; padding:10px ; margin-top:15px" id="alert2" class="alert alert-success" role="alert">
                         <p class="respCarga"></p>
-                        </div>
+                    </div>
+                   
+
                 </form>
             </div>
         </div>
