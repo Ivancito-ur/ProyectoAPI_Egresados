376418f636f4eefb2e5d11d1812209d44a6e88b2
Daniel <danielcaos@ufps.edu.co>
2020-05-26 12:32:06 -0500

    nuevas vistas

---
+++ vista/error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingenieria Agroindustrial</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body style="background-color: #ecf0f5;">
+    <div class="container text-center">
+        <div>
+            <img src="https://img.icons8.com/flat_round/500/000000/error--v1.png" class="img-fluid"
+                alt="Responsive image" style="padding-top: 8%;">
+        </div>
+        <div>
+            <p class="font-weight-bold"><h3>LA DIRECCION A LA QUE DESEA ACCEDER NO EXISTE</h3></p>
+        </div>
+    </div>
+</body>
+
+</html>
